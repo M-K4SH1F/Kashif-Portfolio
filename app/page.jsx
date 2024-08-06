617df00712388ad26b4adbdb5155d5471387f666
Mohammed Kashif Ahmed <159590221+M-K4SH1F@users.noreply.github.com>
2024-08-06 02:09:00 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             </p>
             {/** btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <a href="/assets/Mohammed-Kashif-Ahmed-Resume.pdf" download>
+              <a href="assets/Mohmmed-Kashif-Ahmed-Resume.pdf" download>
                 <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
                   <span>Download Resume</span>
                   <FiDownload className="text-xl" />
