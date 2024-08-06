1dc76045ead8218fbc02d32b614aea35c6e1a15e
Mohammed Kashif Ahmed <159590221+M-K4SH1F@users.noreply.github.com>
2024-08-06 01:58:38 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import{ FiDownload } from "react-icons/fi";
+import { FiDownload } from "react-icons/fi";
 
 //component
 import Social from "@/components/Social";
@@ -11,7 +11,7 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          {/**Text */}
+          {/** Text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer | Graphic Designer</span>
             <h1 className="h1">
@@ -23,17 +23,18 @@ const Home = () => {
             </p>
             {/** btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-                <a href="/assets/Mohmmed Kashif Ahmed Resume.pdf" download/>
-                <span>Download Resume</span>
-                <FiDownload className="text-xl  "/>
-              </Button>
+              <a href="/assets/Mohammed-Kashif-Ahmed-Resume.pdf" download>
+                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                  <span>Download Resume</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0 ">
                 <Social containerStyles="flex gap-6" iconStyles="w-12 h-12 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
               </div>
             </div>
           </div>
-          {/**photo */}
+          {/** photo */}
           <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
