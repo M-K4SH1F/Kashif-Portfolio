c9905b711ca0d2a975f7de53337d37cc7059d5ed
Mohammed Kashif Ahmed <159590221+M-K4SH1F@users.noreply.github.com>
2024-08-09 03:16:58 -0300

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { emailjs } from 'emailjs/browser';
+import emailjs from '@emailjs/browser';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
