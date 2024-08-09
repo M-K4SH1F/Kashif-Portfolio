1ea72aa40752c5482108377f6e2ab76033a192c1
Mohammed Kashif Ahmed <159590221+M-K4SH1F@users.noreply.github.com>
2024-08-09 03:30:00 -0300

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,9 +3,10 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { FaEnvelope, FaMapMarkedAlt } from 'react-icons/fa';
-import { motion } from "framer-motion";
+
+import { FaEnvelope, FaMapMarkedAlt} from 'react-icons/fa';
 
 const info = [
   {
@@ -20,162 +21,102 @@ const info = [
   },
 ];
 
+import { motion } from "framer-motion";
+
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    service: '',
-    message: '',
-  });
+  return <motion.section 
+  initial={{opacity: 0}}
+  animate={{opacity: 1, transition: {delay:2.4, duration: 0.4, ease: "easeIn"},}}
+  className="py-6"
+  >
+    <div className="container mx-auto">
+      <div className="flex flex-col xl:flex-row gap-[30px]">
+        {/**form */}
+        <div className="xl:w-[54%] order-2 xl:order-none">
+          <form className="flex flex-col gap-6 p-10 bg-[#606c38] rounded-xl">
+            <h3 className="text-4xl text-accent font-bold capitalize">Let's work together</h3>
+            <p className="text-[#fefae08e]">
+              Please fill out the form below with your first name, last name, email address, phone number, and a brief message. I look forward to connecting with you and will respond as soon as possible.
+            </p>
 
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [submitStatus, setSubmitStatus] = useState(null);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevData => ({
-      ...prevData,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    try {
-      const result = await emailjs.send(
-        import.meta.env.VITE_APP_EMAILJS_service_g1hh08t,
-        import.meta.env.VITE_APP_EMAILJS_template_hbnhx33,
-        formData,
-        import.meta.env.VITE_APP_EMAILJS_JsJEnFote9POxQPI4
-      );
-
-      console.log(result.text);
-      setSubmitStatus('success');
-      setFormData({
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        service: '',
-        message: '',
-      });
-    } catch (error) {
-      console.error(error.text);
-      setSubmitStatus('error');
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
-  return (
-    <motion.section 
-      initial={{opacity: 0}}
-      animate={{opacity: 1, transition: {delay:2.4, duration: 0.4, ease: "easeIn"},}}
-      className="py-6"
-    >
-      <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-[30px]">
-          <div className="xl:w-[54%] order-2 xl:order-none">
-            <form onSubmit={handleSubmit} className="flex flex-col gap-6 p-10 bg-[#606c38] rounded-xl">
-              <h3 className="text-4xl text-accent font-bold capitalize">Let's work together</h3>
-              <p className="text-[#fefae08e]">
-                Please fill out the form below with your first name, last name, email address, phone number, and a brief message. I look forward to connecting with you and will respond as soon as possible.
-              </p>
-
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input 
-                  type="text"
-                  name="firstName"
-                  placeholder="First Name"
-                  value={formData.firstName}
-                  onChange={handleChange}
-                  required
-                />
-                <Input 
-                  type="text"
-                  name="lastName"
-                  placeholder="Last Name"
-                  value={formData.lastName}
-                  onChange={handleChange}
-                  required
-                />
-                <Input 
-                  type="email"
-                  name="email"
-                  placeholder="Email Address"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-                <Input 
-                  type="tel"
-                  name="phone"
-                  placeholder="Phone Number"
-                  value={formData.phone}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <Select name="service" onValueChange={(value) => handleChange({ target: { name: 'service', value } })}>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Select a service"/>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Select a service</SelectLabel>
-                    <SelectItem value="job-opportunity">Job Opportunity</SelectItem>
-                    <SelectItem value="web-development">Web Development</SelectItem>
-                    <SelectItem value="software-development">Software Development</SelectItem>
-                    <SelectItem value="graphic-design">Graphic Design</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-
-              <Textarea 
-                className="h-[200px]" 
-                placeholder="Type your message here."
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                required
+            {/**input */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <Input 
+                type="firstName"
+                placeholder="First Name"
               />
+              <Input 
+                type="lastName"
+                placeholder="Last Name"
+              />
+              <Input 
+                type="email"
+                placeholder="Email Address"
+              />
+              <Input 
+                type="phone"
+                placeholder="Phone Number"
+              />
+            </div>
 
-              <Button type="submit" size="md" className="max-w-40" disabled={isSubmitting}>
-                {isSubmitting ? 'Sending...' : 'Send Message'}
-              </Button>
+            {/**Select */}
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select a service"/>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>
+                    Select a service
+                  </SelectLabel>
+                  <SelectItem value="job-opportunity">
+                    Job Opportunity
+                  </SelectItem>
+                  <SelectItem value="web-development">
+                    Web Development
+                  </SelectItem>
+                  <SelectItem value="software-development">
+                    Software Development
+                  </SelectItem>
+                  <SelectItem value="graphic-design">
+                    Graphic Design
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
 
-              {submitStatus === 'success' && (
-                <p className="text-green-500">Message sent successfully!</p>
-              )}
-              {submitStatus === 'error' && (
-                <p className="text-red-500">Failed to send message. Please try again.</p>
-              )}
-            </form>
-          </div>
+            {/**textarea */}
+            <Textarea className="h-[200px]" placeholder="Typer your message here."/>
 
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            <ul className="flex flex-col gap-16">
-              {info.map((item, index) => (
+            {/**button */}
+            <Button size="md" className="max-w-40">Send Message</Button>
+          
+
+          </form>
+        </div>
+
+        {/**info */}
+        <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+          <ul className="flex flex-col gap-16">
+            {info.map((item, index)=> {
+              return (
                 <li key={index} className="flex items-center gap-6">
                   <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#606C38] text-accent-hover rounded-md flex items-center justify-center">
-                    <div className="text-[28px]">{item.icon}</div>
+                    <div className-="text-[28px]">{item.icon}</div>
                   </div>
                   <div className="flex-1">
                     <p className="text-white/60">{item.title}</p>
                     <h3 className="text-xl">{item.description}</h3>
                   </div>
                 </li>
-              ))}
-            </ul>
-          </div>
+              );
+            })}
+          </ul>
         </div>
+
       </div>
-    </motion.section>
-  );
+    </div>
+  </motion.section>;
 };
 
 export default Contact;
